195e2b4ecb58d63e9063e6c9fef437589f28fbb8
cvbn55688 <han@DESKTOP-BRA2HOF>
2025-04-18 00:46:57 +0800

    feat(php): initbash: line 1: wq: command not foun

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # php-test
+```shell
+sudo apt update
+sudo apt install mysql-server
+sudo apt install php php-cli php-mysql
+
+
+sudo systemctl status mysql
+sudo systemctl start mysql
+
+
+
+# If System has not been booted with systemd as init system
+sudo /usr/sbin/mysqld &
+
+# If have permission problem
+sudo mkdir -p /var/run/mysqld
+sudo chown mysql:mysql /var/run/mysqld
+sudo chmod 755 /var/run/mysqld
+
+
+```
---
+++ register.php
@@ -0,0 +1,32 @@
+<?php
+$host = 'localhost';
+$dbname = 'test';
+$user = 'root';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user);
+} catch (PDOException $e) {
+    die("資料庫連線失敗：" . $e->getMessage()); 
+} 
+
+$username = $_POST['username']?? '';
+$password = $_POST['password'] ?? ''; 
+
+if ($username && $password) {
+    $stmt = $pdo->prepare("SELECT COUNT(*) FROM users WHERE username = ?");
+    $stmt->execute([$username]);
+    $count = $stmt->fetchColumn();
+
+    if ($count > 0) {
+        echo "<script>alert('註冊失敗：帳號已存在'); window.location.href='fail.html';</script>";
+        exit;
+    }
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    $stmt = $pdo->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    if ($stmt->execute([$username, $hashedPassword])) {
+        echo "<script>alert('註冊成功'); window.location.href='success.html';</script>";
+    }
+} else {
+    echo "<script>alert('錯誤');</script>";
+}
+?>
